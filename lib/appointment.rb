a58337f63a0cbe09f2875616ca94b7bca48f4995
Rebecca Congi <rncongi@gmail.com>
2020-08-25 21:31:53 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,10 +1,14 @@
 class Appointment
   
-  
+  attr_reader :date, :patient, :doctor 
   
   @@all = []
   
-  def initialize()
+  def initialize(date, patient, doctor)
+    @date = date 
+    @patient = patient 
+    @doctor = doctor
+    
     @@all << self 
   end   
   
